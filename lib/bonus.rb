576c04c8980b6f5c4f057771cfd2c62b76428359
Anette Rivera <anettem.rivera@gmail.com>
2020-05-08 21:56:16 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -23,7 +23,7 @@ def bonus
   #code your solution here:
   
   epic_tragedy[:montague][:hero][:status] = "dead"
-  expect(bonus[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
 
   
